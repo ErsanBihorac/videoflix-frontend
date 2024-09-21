3175398c476c3ff49e8df5655d3968bc444be6d9
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2024-09-21 06:35:03 +0200

    switched forgot p and reset p components

--- src/app/components/forgot-p/forgot-p.component.ts
+++ src/app/components/forgot-p/forgot-p.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { LoginBtnComponent } from "../login-btn/login-btn.component";
+import { EmailInputComponent } from "../email-input/email-input.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-p',
   standalone: true,
-  imports: [],
+  imports: [LoginBtnComponent, EmailInputComponent, RouterLink],
   templateUrl: './forgot-p.component.html',
   styleUrl: './forgot-p.component.scss'
 })
 export class ForgotPComponent {
+  err_msg_active: boolean = false;
+  email: string = '';
 
+  onEmailChange(value: string) {
+    this.email = value;
+  }
+
+  switchErrorMessage() {
+    this.err_msg_active ? this.err_msg_active = false : this.err_msg_active = true;
+  }
 }

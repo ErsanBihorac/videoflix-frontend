30f41e20b49d7b33f9f89c1726a6e49a368a4c41
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2024-09-21 06:50:10 +0200

    created reset password page

--- src/app/components/reset-p/reset-p.component.ts
+++ src/app/components/reset-p/reset-p.component.ts
@@ -1,13 +1,56 @@
 import { Component } from '@angular/core';
+import { LoginBtnComponent } from "../login-btn/login-btn.component";
+import { EmailInputComponent } from "../email-input/email-input.component";
+import { PasswordInputComponent } from "../password-input/password-input.component";
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-reset-p',
   standalone: true,
-  imports: [RouterLink],
+  imports: [LoginBtnComponent, EmailInputComponent, PasswordInputComponent, RouterLink],
   templateUrl: './reset-p.component.html',
   styleUrl: './reset-p.component.scss'
 })
 export class ResetPComponent {
+  err_msg_active: boolean = false;
+  password: string = '';
+  confirm_password: string = '';
 
+  password_type: string = 'password';
+  confirm_password_type: string = 'password';
+
+  password_visibility: string = 'img/visibility.svg';
+  confirm_password_visibility: string = 'img/visibility.svg';
+
+  onPasswordChange(value: string) {
+    this.password = value;
+  }
+
+  onConfirmPasswordChange(value: string) {
+    this.confirm_password = value;
+  }
+
+  switchPasswordVisibility() {
+    if (this.password_type === 'password') {
+      this.password_type = 'text';
+      this.password_visibility = 'img/visibility_off.svg';
+    } else {
+      this.password_type = 'password';
+      this.password_visibility = 'img/visibility.svg';
+    }
+  }
+
+  switchConfirmPasswordVisibility() {
+    if (this.confirm_password_type === 'password') {
+      this.confirm_password_type = 'text';
+      this.confirm_password_visibility = 'img/visibility_off.svg';
+    } else {
+      this.confirm_password_type = 'password';
+      this.confirm_password_visibility = 'img/visibility.svg';
+    }
+  }
+
+  switchErrorMessage() {
+    this.err_msg_active ? this.err_msg_active = false : this.err_msg_active = true;
+  }
 }

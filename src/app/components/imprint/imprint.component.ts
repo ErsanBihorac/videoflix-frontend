8afd86376254c25a153b0ebfe5c5827e5f014a19
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2024-09-22 07:44:12 +0200

    created privacy policy and imprint

--- src/app/components/imprint/imprint.component.ts
+++ src/app/components/imprint/imprint.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LoginBtnComponent } from "../login-btn/login-btn.component";
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [],
+  imports: [LoginBtnComponent],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
